3713b9bfa6f928dc47769d4342997161f0bd4fb0
王耀 <44947737@qq.com>
2018-10-17 08:04:04 +0800

    deldete all

--- src/main/java/com/jqm/ssm/misc/es/JRedisUtil.java
+++
@@ -1,8 +0,0 @@
-package com.jqm.ssm.misc.es;
-
-/**
- * Created by Administrator on 2018/7/27.
- */
-public class JRedisUtil {
-
-}
--- src/test/java/other/DocGenerator.java
+++
@@ -1,9 +0,0 @@
-package other;
-
-/**
- * 反射生成文件
- * Created by wangyaoyao on 2018/8/15.
- */
-public class DocGenerator {
-
-}
